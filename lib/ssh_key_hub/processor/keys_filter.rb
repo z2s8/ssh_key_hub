38d0fa4b2c43ab6cd33aaf3df4582e4e5b3d9dea
z2s8 <szanto.zsombor@gmail.com>
2017-02-16 23:12:58 +0100

    Add key filterer
    keys could be white- or black-listed based on type and size (partly done)

---
+++ lib/ssh_key_hub/processor/keys_filter.rb
@@ -0,0 +1,49 @@
+require_relative 'key_processor'
+
+module Processor
+  # SSH public key filter
+  class KeysFilter
+    @infinity = 1.0 / 0.0
+    # @param [Hash] credentials Hash with keys by username with +SortedSet+s
+    def initialize(credentials={})
+      @credentials = credentials
+    end
+
+    # Add new credentials to the filter
+    # @param [Hash] new_creds Hash with keys by username with +SortedSet+s
+    def add(new_creds)
+      @credentials.merge!(new_creds) { |_, old_val, new_val| old_val + new_val }
+    end
+
+    # TODO
+    def allow(type, min_bits, max_bits=@infinity)
+    end
+
+    # TODO
+    def reject(type, min_bits=0, max_bits)
+    end
+
+    # Remove weak keys from credentials
+    # Currently: any DSA, RSA below 2048 bits, and EC below 256 bits
+    # @return [Hash] credentials Hash with keys by username with +SortedSet+s
+    def reject_weak
+      @credentials.each do |user, keys|
+        keys.delete_if do |key|
+          type, bits = Processor::KeyProcessor.new.key_type_and_bits(key)
+          puts "testing #{type} with #{bits}"
+          case type
+          when :DSA
+            true
+          when :RSA
+            bits < 2048
+          when :EC
+            bits < 256
+          else
+            true
+          end
+        end
+      end
+      @credentials
+    end
+  end
+end
